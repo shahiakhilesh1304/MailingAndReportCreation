7f8b9f2e18cf24d4d07538ff79ce06ca1c83aa7e
ashahi <ashahi@mphrx.com>
2023-02-17 12:18:02 +0530

    Final Deployment

--- config.py
+++ config.py
@@ -9,22 +9,43 @@ from email.mime.base import MIMEBase
 from email import encoders
 from email.mime.text import MIMEText
 
-class MongoDetailConstants:
+os.system('source /etc/mphrx/cust_env.sh')
 
+class MongoDetailConstants:
     def __init__(self):
-        self.custId = os.environ.get('CUSTID')
-        if not self.custId:
-            raise ValueError("CUSTID environment variable not set")
         self.envType = os.environ.get('ENVIRONMENT_TYPE')
         if not self.envType:
             raise ValueError("ENVIRONMENT_TYPE environment variable not set")
-        self._IP = os.environ.get('DB_HOSTNAME')
-        if not self._IP:
-            raise ValueError("DB_HOSTNAME environment variable not set")
         self._PORT = "27018"
-        self._USERNAME = "{0}_minerva".format(self.custId)
-        self._PASSWORD = "{0}_minerva".format(self.custId)
-        self._DATABASE = "{0}_minervadb".format(self.custId)
+        self.custId = os.environ.get('CUSTID')
+        if not self.custId:
+            raise ValueError("CUSTID environment variable not set")
+
+
+        if self.envType.upper() == "DEV":
+            self._IP = "10.31.10.68"
+            if not self._IP:
+                raise ValueError("DB_HOSTNAME environment variable not set")
+            self._USERNAME = "unimdev_minerva"
+            self._PASSWORD = "unimdev_minerva"
+            self._DATABASE = "unimdev_minervadb"
+        elif self.envType.upper() == "QA":
+            self._IP = "10.31.10.69"
+            if not self._IP:
+                raise ValueError("DB_HOSTNAME environment variable not set")
+            self._USERNAME = "unimqa_minerva"
+            self._PASSWORD = "unimqa_minerva"
+            self._DATABASE = "unimqa_minervadb"
+        elif self.envType.upper() == "UAT":
+            self._IP = "10.31.10.70"
+            if not self._IP:
+                raise ValueError("DB_HOSTNAME environment variable not set")
+            self._USERNAME = "uniuat_minerva"
+            self._PASSWORD = "uniuat_minerva"
+            self._DATABASE = "uniuat_minervadb"
+        else:
+            raise ValueError(f"The DB Credentials are not supported for this environment named as {self.envType}, Connect with the developer to include this support")
+            os.exit()
 
     @property
     def IP(self):
@@ -78,15 +99,23 @@ def establishing_Connection():
     print("Connection Established")
     return client
 
+def getDB():
+    conDetail = connection_Details()
+    client = establishing_Connection()
+    return client[conDetail.DATABASE]
+
 def append_to_csv(fileName, data):
     with open(fileName, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(data)
 
-def get_Csv(data,filename):
+def get_Csv(data,filename, filepath):
     # file_Data={"<strong>SL No.</strong>":" ","<strong>Data</strong>":" ","<strong>Numero da Carteirinha</strong>":" ","<strong>CPF</strong>":" ","<strong>Nome</strong>":" ","<strong>Data de nascimento</strong>":" "}
     fields = ["SL No.","Data","Numero da Carteirinha","CPF","Nome","Data de nascimento"]
     count = 0;
+    filename = os.path.join(filepath,filename)
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
     try:
         with open(filename, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
@@ -111,9 +140,10 @@ def extract_Card_Number(data):
             cardNumbers.add(i.get("idValue"))
     return cardNumbers
 
-def sendEmail(filename):
+def sendEmail(filename,path):
     mailAttachment = []
-    data = open(filename,'rb').read()
+    filname_with_path = os.path.join(path,filename)
+    data = open(filname_with_path,'rb').read()
     base64_encoded_invalidCPF = base64.b64encode(data).decode('UTF-8')
     invalidCPFAttachment = {}
     invalidCPFAttachment["attachmentName"] = filename
@@ -121,10 +151,10 @@ def sendEmail(filename):
     invalidCPFAttachment["derefUri"] = base64_encoded_invalidCPF
     mailAttachment.append(invalidCPFAttachment)
     subject = "Relatório Semanal: Relatório de CPF Inválido"
-    emailIds = ["ashahi@mphrx.com"] # Comment For Production
+    emailIds = ["ashahi@mphrx.com","ssharma6@mphrx.com"] # Comment For Production
     # emailIds = ["res@unimed.coop.br"] # Uncomment For Production
     body = f"Olá, pessoal\nEsperamos que vocês estejam bem, por favor consulte o anexo para ver a lista de CPFs inválidos identificados durante a ingestão de dados de {datetime.now()-timedelta(days=7)} para {datetime.now()}.\n\nObrigado e Cumprimentos\nMphRx"
-    mail_from = "test_mphrx_unimed@hotmail.com"
+    mail_from = "ashahi@mphrx.com"
     message = MIMEMultipart()
     emailResponse = dict()
     for mail_id in emailIds:
@@ -132,8 +162,8 @@ def sendEmail(filename):
         message["To"] = mail_id
         message["Subject"] = subject
         message.attach(MIMEText(body))
-        with open(filename,'rb') as file:
-            part = MIMEBase("application","octet-stream")
+        with open(filname_with_path,'rb') as file:
+            part = MIMEBase("application","octet-stream",Name = filename)
             part.set_payload(file.read())
         encoders.encode_base64(part)
         part.add_header(
@@ -144,7 +174,7 @@ def sendEmail(filename):
         smtp.ehlo()
         smtp.starttls()
         smtp.ehlo()
-        smtp.login(mail_from,"TestUnimed@123")
+        smtp.login(mail_from,"Mphrx@1234")
         response = smtp.sendmail(mail_from,mail_id,message.as_string())
         smtp.close()
         if response:
--- main.py
+++ main.py
@@ -2,28 +2,29 @@ import config as config
 import os
 from datetime import datetime,timedelta
 
-client = config.establishing_Connection()
-db = client["unimdev_minervadb"]
+db = config.getDB()
 collection = db["customCPFStatus"]
 endTime = datetime.now()
 startTime = datetime.now() - timedelta(days=7)
 data = collection.find({"dateCreated": {'$gte': startTime, '$lte': endTime}})
 sT= startTime.strftime("%d-%m-%Y")
 eT = endTime.strftime("%d-%m-%Y")
-
+path = os.path.join("tmp","FailedCPFReport")
+new_path = os.path.join(path,"CSVFile")
 filename = f"Invalid_CPF_Report({sT}_to_{eT}).csv"
-print(filename)
 
 
 try:
-    os.remove(filename)
+    os.remove(os.path.join(path,filename))
     print("File removed successfully")
 except FileNotFoundError:
     print("File not found")
 except:
     print("Error deleting file")
 
-config.get_Csv(data, filename)
-status = config.sendEmail(filename)
+config.get_Csv(data, filename, new_path)
+print("File Created Ready For Email...")
+print("Emailing the file...")
+status = config.sendEmail(filename,new_path)
 print(status)
 
